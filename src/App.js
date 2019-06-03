a85c0b52c3a201e8a806f5d19c62544450458549
macaperez <macaperezcarrillo@gmail.com>
2019-06-03 20:48:21 +0100

    Crear App y Añadir Sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles-maca.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App maca">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="maca">
+          <h2 className="maca2">Macarena</h2>
+        </div>
       </header>
     </div>
   );
